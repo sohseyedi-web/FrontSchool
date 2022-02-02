1d6fe595434497a536d0fd280918aaaffeacba0a
sohseyedi <sohseyedi@gmail.com>
2022-02-02 14:32:50 +0330

    create roadmap

--- src/Components/Home/Intro.jsx
+++ src/Components/Home/Intro.jsx
@@ -6,7 +6,51 @@ const Intro = () => {
         <section className='intro'>
             <div className="intro-title">roadmap to becoming a full stack developer</div>
             <div className="intro-content container">
-
+                <div className="intro-content__block">
+                    <div className="intro-content__block-title">
+                        <h6>Learn Html , Css</h6>
+                        <span>Step 1</span>
+                    </div>
+                    <p className="intro-content__block-body">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aperiam ea vero delectus temporibus beatae.
+                    </p>
+                </div>
+                <div className="intro-content__block">
+                    <div className="intro-content__block-title">
+                        <h6>Learn JavaScript</h6>
+                        <span>Step 2</span>
+                    </div>
+                    <p className="intro-content__block-body">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aperiam ea vero delectus temporibus beatae.
+                    </p>
+                </div>
+                <div className="intro-content__block">
+                    <div className="intro-content__block-title">
+                        <h6>Learn Javascript Framework</h6>
+                        <span>Step 3</span>
+                    </div>
+                    <p className="intro-content__block-body">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aperiam ea vero delectus temporibus beatae.
+                    </p>
+                </div>
+                <div className="intro-content__block">
+                    <div className="intro-content__block-title">
+                        <h6>Learn NodeJs | Python | ...</h6>
+                        <span>Step 4</span>
+                    </div>
+                    <p className="intro-content__block-body">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aperiam ea vero delectus temporibus beatae.
+                    </p>
+                </div>
+                <div className="intro-content__block">
+                    <div className="intro-content__block-title">
+                        <h6>Learn Database</h6>
+                        <span>Step 5</span>
+                    </div>
+                    <p className="intro-content__block-body">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aperiam ea vero delectus temporibus beatae.
+                    </p>
+                </div>
             </div>
         </section>
     );
